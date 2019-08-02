7c97840bba7b20e9b6f9a5bf22cf8aaee2bb71d1
Raman Mangla <ramanmangla7@gmail.com>
2019-08-02 19:58:55 -0400

    CSS changes and colour theme changes

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,4 @@
+
 // Starting from the top whenever page is refreshed
 // 20ms delay is used because browsers tend to go
 // back to last scroll position after the page is loaded
@@ -6,87 +7,64 @@
 // callback after a long delay
 
 $(document).ready(function() {
-  $("html,body").animate(
-    {
-      scrollTop: 0
-    },
-    "fast",
-    function() {
-      setTimeout(function() {
-        $(".loader").fadeOut(600, function() {
-          $("body").css("overflow", "visible");
-        });
-      }, 1400);
-    }
-  );
+	$('html,body').animate({
+		scrollTop: 0
+	}, 'fast', function() {
+		setTimeout(function() {
+			$('.loader').fadeOut(600, function() {
+				$('body').css('overflow', 'visible');
+			});
+		}, 2800);
+	});
 
-  var lastScroll = 0;
+	var lastScroll = 0;
 
-  $(window).scroll(function() {
-    if ($(window).scrollTop() > 220) {
-      $("header").addClass("shadowHeader");
+	$(window).scroll(function() {
 
-      if ($(window).scrollTop() < lastScroll) {
-        $("header").removeClass("hideHeader");
-      } else {
-        $("header").addClass("hideHeader");
-      }
-    } else {
-      $("header").removeClass("shadowHeader");
-      $("header").removeClass("hideHeader");
-    }
+		if($(window).scrollTop() > 150) {
+			$('header').addClass('shadowHeader');
 
-    lastScroll = $(window).scrollTop();
-  });
+			if($(window).scrollTop() < lastScroll) {
+				$('header').removeClass('hideHeader');
+			} else {
+				$('header').addClass('hideHeader');
+			}
+		} else {
+			$('header').removeClass('shadowHeader');
+			$('header').removeClass('hideHeader');
+		}
+
+		lastScroll = $(window).scrollTop();
+	});
 });
 
 // Smooth transition to next page
 // when navigation link is clicked
-$("header nav a").on("click", function(e) {
-  var targetHref = $(this).attr("href");
-  var offset = 0;
+$('header nav a').on('click', function(e) {
+	var targetHref = $(this).attr('href');
+	var offset = 0;
 
-  offset = targetHref == "#Projects" ? -40 : 0;
+	offset = targetHref == '#Projects'? -40:0;
 
-  $("html, body").animate(
-    {
-      scrollTop: $(targetHref).offset().top + offset
-    },
-    1000
-  );
+	$('html, body').animate({
+		scrollTop: $(targetHref).offset().top + offset
+	}, 1000);
 
-  e.preventDefault();
+	e.preventDefault();
 });
-
-// Current active experience
-var expTargetID = $("a[href='#Publicis']");
 
 // Experiences carousel
-$(".experiences nav a").on("click", function(e) {
-  e.preventDefault();
+$('.experiences nav a').on('click', function(e) {
+	e.preventDefault();
 
-  $(expTargetID).removeClass("activeExperience");
+	var targetHref = $(this).attr('href');
 
-  expTargetID = $(this);
+	$('.experiences nav a').css('color', '#47CFF9');
+	$(this).css('color', '#FFFFFF');
 
-  $(expTargetID).addClass("activeExperience");
-
-  $(".experiences > div").animate(
-    {
-      scrollLeft:
-        $(".experiences > div").scrollLeft() +
-        $(expTargetID.attr("href")).position().left
-    },
-    600
-  );
-});
-
-$(window).resize(function() {
-  var container = $(".experiences > div");
-
-  container.scrollLeft(
-    container.scrollLeft() + $(expTargetID.attr("href")).position().left
-  );
+	$('.experiences > div').animate({
+		scrollLeft: $('.experiences > div').scrollLeft() + $(targetHref).position().left
+	}, 600);
 });
 
 // $('#AboutLink').click(function() {
@@ -96,21 +74,21 @@ $(window).resize(function() {
 // });
 
 // Responsive navigation
-$("#navIcon").on("click", function() {
-  $(".navLinks").css("right", 0);
+$('#navIcon').on('click', function() {
+	$('.navLinks').css('right', 0);
 
-  $(".navLinks a").on("click", function() {
-    $(".navLinks").css("right", "-200px");
-  });
+	$('.navLinks a').on('click', function(){
+		$('.navLinks').css('right', '-200px');
+	});
 
-  $(window).resize(function() {
-    $(".navLinks").css("right", "-200px");
-  });
+	$(window).resize(function() {
+  		$('.navLinks').css('right', '-200px');
+	});
 
-  $(document).mouseup(function(e) {
-    var menu = $(".navLinks");
-    if (!menu.is(e.target) && menu.has(e.target).length === 0) {
-      menu.css("right", "-200px");
-    }
-  });
+	$(document).mouseup(function(e){
+		var menu = $('.navLinks');
+	    if (!menu.is(e.target) && menu.has(e.target).length === 0) {
+     		menu.css('right', '-200px');
+   		}
+	});
 });
